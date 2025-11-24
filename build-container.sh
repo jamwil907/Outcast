4c2df4a28ebad144066c8f73fe150655fd070454
jamwil907 <jamwil907@gmail.com>
2025-11-24 04:17:37 -0500

    Update build-container.sh  to edit docker run command

--- build-container.sh
+++ build-container.sh
@@ -6,13 +6,13 @@ set -e
 echo "=== PHP Lint ==="
 
 # Run lint checks using PHP official Docker image
-docker run --rm -v "$PWD":/app -w /app php:8.2-cli php -l index.php
-docker run --rm -v "$PWD":/app -w /app php:8.2-cli php -l includes/functions.php
-docker run --rm -v "$PWD":/app -w /app php:8.2-cli php -l includes/config.php
-docker run --rm -v "$PWD":/app -w /app php:8.2-cli php -l login.php
-docker run --rm -v "$PWD":/app -w /app php:8.2-cli php -l register.php
-docker run --rm -v "$PWD":/app -w /app php:8.2-cli php -l logout.php
-docker run --rm -v "$PWD":/app -w /app php:8.2-cli php -l test_weather.php
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/app php:8.2-cli php -l index.php
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/app php:8.2-cli php -l includes/functions.php
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/app php:8.2-cli php -l includes/config.php
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/app php:8.2-cli php -llogin.php
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/app php:8.2-cli php -l register.php
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/app php:8.2-cli php -l logout.php
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/app php:8.2-cli php -l test_weather.php
 
 echo "=== Quick Weather Test ==="
 docker run --rm -v "$PWD":/app -w /app php:8.2-cli php test_weather.php || \
